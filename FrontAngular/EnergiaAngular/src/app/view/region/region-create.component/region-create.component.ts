9fce4da056e0258c15e1101e266b70ec72a212c0
Blawer <blawermoncada@gmail.com>
2026-03-05 21:04:08 -0500

    Primera iteraccion de region

---
+++ FrontAngular/EnergiaAngular/src/app/view/region/region-create.component/region-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-region-create.component',
+  imports: [],
+  templateUrl: './region-create.component.html',
+  styleUrl: './region-create.component.css',
+})
+export class RegionCreateComponent {
+
+}
---
+++ FrontAngular/EnergiaAngular/src/app/view/region/region-list.component/region-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-region-list.component',
+  imports: [],
+  templateUrl: './region-list.component.html',
+  styleUrl: './region-list.component.css',
+})
+export class RegionListComponent {
+
+}
