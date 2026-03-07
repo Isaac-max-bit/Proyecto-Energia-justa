2335aea933382e5180eecbbc6ceffafebf688b13
smuneram98 <smm980531@gmail.com>
2026-03-07 18:23:09 -0500

    algunos cambios

--- ProyectoEnergia/energia/src/main/java/com/equipo17/energia/dto/LoginRequest.java
+++ ProyectoEnergia/energia/src/main/java/com/equipo17/energia/dto/LoginRequest.java
@@ -7,22 +7,4 @@ public class LoginRequest {
     private String email;
     private String password;
 
-/*     public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public CharSequence getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    } */
-
-
-
 }
