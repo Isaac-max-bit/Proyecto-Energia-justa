4f645960a3f7862fe9f02950fc89698334d0016a
smuneram98 <smm980531@gmail.com>
2026-03-08 23:13:05 -0500

    routes update

--- FrontAngular/EnergiaAngular/src/app/app.routes.ts
+++ FrontAngular/EnergiaAngular/src/app/app.routes.ts
@@ -6,11 +6,15 @@ import { RegionComponent } from './view/region/region.component/region.component
 import { PowerPlantComponent } from './view/power-plant/power-plant.component/power-plant.component';
 import { CompanyComponent } from './view/company/company.component/company.component';
 import { EnergyTypeComponent } from './view/energy/energy-type.component/energy-type.component';
+import { UserComponent } from './view/user/user.component/user.component';
+import { EnergyComponent } from './view/energy/energy.component/energy.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'user', component: UserComponent },
+    { path: 'energy', component: EnergyComponent },
     {path: 'country', component: CountryComponent},
     {path: 'region', component: RegionComponent},
     {path: 'company', component: CompanyComponent},
